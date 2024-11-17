5396d6e5bdbb25e6bc10913513375fe91451f263
Jack Aubrey <pacificland68@hotmail.com>
2024-11-17 22:05:48 +0800

    new

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,21 +1,45 @@
 <template>
 	<view class="login-container">
-	    <view class="form-group">
-	      <input class="input" placeholder="请输入邮箱或者手机号" v-model="account" />
-	    </view>
-	    <view class="form-group">
-	      <input class="input" type="password" placeholder="请输入密码" v-model="password" />
-	    </view>
-		<view class="form-group">
-			<input v-model="code" placeholder="请输入验证码" size="large" @search="authCaptcha"></input>
-			<span class="loginc_img">
-			    <img :src="codeImage" alt="" v-if="codeImage" @click="authCaptcha" />
-			    <span v-else @click="authCaptcha">
-			        <i class="iconfont icon-refresh"></i>
-					{{ $t('刷新') }}
-			    </span>
-			</span>
-		</view>
+	    <view class="form-group" :class="{ 'has-error': errors.account }">
+	          <input
+	            class="input"
+	            placeholder="请输入邮箱或者手机号"
+	            v-model="account"
+	            @input="clearError('account')"
+	          />
+	          <view v-if="errors.account" class="error-text">{{ errors.account }}</view>
+	        </view>
+	    
+	        <!-- 密码 -->
+	        <view class="form-group" :class="{ 'has-error': errors.password }">
+	          <input
+	            class="input"
+	            type="password"
+	            placeholder="请输入密码"
+	            v-model="password"
+	            @input="clearError('password')"
+	          />
+	          <view v-if="errors.password" class="error-text">{{ errors.password }}</view>
+	        </view>
+	    
+	        <!-- 验证码 -->
+	        <view class="form-group" :class="{ 'has-error': errors.code }">
+	          <input
+	            v-model="code"
+	            placeholder="请输入验证码"
+	            size="large"
+	            @input="clearError('code')"
+	          />
+	          <span class="loginc_img">
+	            <img :src="codeImage" alt="" v-if="codeImage" @click="authCaptcha" />
+	            <span v-else @click="authCaptcha">
+	              <i class="iconfont icon-refresh"></i>
+	              刷新
+	            </span>
+	          </span>
+	          <view v-if="errors.code" class="error-text">{{ errors.code }}</view>
+	        </view>
+
 	    <button class="btn" @click="handleLogin">登录</button>
 		<view>
 			<span>还没有账号？</span><button @click="jumpRegister">注册</button>
@@ -28,11 +52,46 @@
 	import { login, authCaptcha } from '../../api/auth';
 	import isPhoneOrEmail from '@/utils/index.js'
 
-	let account = ref();
-	let password = ref();
-	let code = ref()
+	let account = ref("");
+	let password = ref("");
+	let code = ref("")
 	let codeImage = ref()
 	let uuid = ref()
+	let errors = ref({
+		account: "",
+		password: "",
+		code: ""
+	})
+	
+	// 清除某个错误提示
+	const clearError = (field) => {
+		errors.value[field] = ""
+	}
+	
+	// 表单校验
+	const validateForm = () => {
+	      let valid = true;
+	
+	      // 校验邮箱或手机号
+	      if (!account.value.trim()) {
+	        errors.value.account = "邮箱或者手机号不能为空";
+	        valid = false;
+	      }
+	
+	      // 校验密码
+	      if (!password.value.trim()) {
+	        errors.value.password = "密码不能为空";
+	        valid = false;
+	      }
+	
+	      // 校验验证码
+	      if (!this.code.trim()) {
+	        errors.value.code = "验证码不能为空";
+	        valid = false;
+	      }
+	
+	      return valid;
+	    }
 	
 	// 获取验证码
 	authCaptcha().then(res => {
@@ -62,35 +121,30 @@
 	// }
 	
 	const handleLogin = () => {
-		if(!account || !password){
-			uni.showToast({
-				title: "请输入用户名和密码",
-				icon: 'none'
-			})
-			return
-		}
 		
-		let params = {
-			account: account.value,
-			password: password.value,
-			code: code.value,
-			loginType: isPhoneOrEmail(account.value),
-			uuid: uuid.value
-		}
-		
-		if(isPhoneOrEmail(account.value) === 1){
-			params['email'] = account.value
-		}else if(isPhoneOrEmail(account.value) === 2){
-			params['phoneNumber'] = account.value
-		}
-		
-		login(params).then(res => {
-			if(res.code === 200){
-				uni.redirectTo({
-					url:"/pages/index/index"
-				})
+		if(validateForm()){
+			let params = {
+				account: account.value,
+				password: password.value,
+				code: code.value,
+				loginType: isPhoneOrEmail(account.value),
+				uuid: uuid.value
 			}
-		})
+			
+			if(isPhoneOrEmail(account.value) === 1){
+				params['email'] = account.value
+			}else if(isPhoneOrEmail(account.value) === 2){
+				params['phoneNumber'] = account.value
+			}
+			
+			login(params).then(res => {
+				if(res.code === 200){
+					uni.redirectTo({
+						url:"/pages/index/index"
+					})
+				}
+			})
+		}
 	}
 </script>
 
@@ -114,5 +168,15 @@
   color: #fff;
   text-align: center;
   border-radius: 4px;
+}
+
+.form-group.has-error .input {
+  border-color: red;
+}
+
+.error-text {
+  color: red;
+  font-size: 12px;
+  margin-top: 5px;
 }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/login/login.js
+++ unpackage/dist/dev/mp-weixin/pages/login/login.js
@@ -5,11 +5,35 @@ const utils_index = require("../../utils/index.js");
 const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
   __name: "login",
   setup(__props) {
-    let account = common_vendor.ref();
-    let password = common_vendor.ref();
-    let code = common_vendor.ref();
+    let account = common_vendor.ref("");
+    let password = common_vendor.ref("");
+    let code = common_vendor.ref("");
     let codeImage = common_vendor.ref();
     let uuid = common_vendor.ref();
+    let errors = common_vendor.ref({
+      account: "",
+      password: "",
+      code: ""
+    });
+    const clearError = (field) => {
+      errors.value[field] = "";
+    };
+    const validateForm = () => {
+      let valid = true;
+      if (!account.value.trim()) {
+        errors.value.account = "邮箱或者手机号不能为空";
+        valid = false;
+      }
+      if (!password.value.trim()) {
+        errors.value.password = "密码不能为空";
+        valid = false;
+      }
+      if (!this.code.trim()) {
+        errors.value.code = "验证码不能为空";
+        valid = false;
+      }
+      return valid;
+    };
     api_auth.authCaptcha().then((res) => {
       uuid.value = res.uuid;
       codeImage.value = "data:image/gif;base64," + res.img;
@@ -20,61 +44,66 @@ const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
       });
     };
     const handleLogin = () => {
-      if (!account || !password) {
-        common_vendor.index.showToast({
-          title: "请输入用户名和密码",
-          icon: "none"
-        });
-        return;
-      }
-      let params = {
-        account: account.value,
-        password: password.value,
-        code: code.value,
-        loginType: utils_index.isPhoneOrEmail(account.value),
-        uuid: uuid.value
-      };
-      if (utils_index.isPhoneOrEmail(account.value) === 1) {
-        params["email"] = account.value;
-      } else if (utils_index.isPhoneOrEmail(account.value) === 2) {
-        params["phoneNumber"] = account.value;
-      }
-      api_auth.login(params).then((res) => {
-        if (res.code === 200) {
-          common_vendor.index.redirectTo({
-            url: "/pages/index/index"
-          });
+      if (validateForm()) {
+        let params = {
+          account: account.value,
+          password: password.value,
+          code: code.value,
+          loginType: utils_index.isPhoneOrEmail(account.value),
+          uuid: uuid.value
+        };
+        if (utils_index.isPhoneOrEmail(account.value) === 1) {
+          params["email"] = account.value;
+        } else if (utils_index.isPhoneOrEmail(account.value) === 2) {
+          params["phoneNumber"] = account.value;
         }
-      });
+        api_auth.login(params).then((res) => {
+          if (res.code === 200) {
+            common_vendor.index.redirectTo({
+              url: "/pages/index/index"
+            });
+          }
+        });
+      }
     };
     return (_ctx, _cache) => {
       return common_vendor.e({
-        a: common_vendor.unref(account),
-        b: common_vendor.o(($event) => common_vendor.isRef(account) ? account.value = $event.detail.value : account = $event.detail.value),
-        c: common_vendor.unref(password),
-        d: common_vendor.o(($event) => common_vendor.isRef(password) ? password.value = $event.detail.value : password = $event.detail.value),
-        e: common_vendor.o(
-          //@ts-ignore
-          (...args) => common_vendor.unref(api_auth.authCaptcha) && common_vendor.unref(api_auth.authCaptcha)(...args)
-        ),
-        f: common_vendor.unref(code),
-        g: common_vendor.o(($event) => common_vendor.isRef(code) ? code.value = $event.detail.value : code = $event.detail.value),
-        h: common_vendor.unref(codeImage)
+        a: common_vendor.o([($event) => common_vendor.isRef(account) ? account.value = $event.detail.value : account = $event.detail.value, ($event) => clearError("account")]),
+        b: common_vendor.unref(account),
+        c: common_vendor.unref(errors).account
+      }, common_vendor.unref(errors).account ? {
+        d: common_vendor.t(common_vendor.unref(errors).account)
+      } : {}, {
+        e: common_vendor.unref(errors).account ? 1 : "",
+        f: common_vendor.o([($event) => common_vendor.isRef(password) ? password.value = $event.detail.value : password = $event.detail.value, ($event) => clearError("password")]),
+        g: common_vendor.unref(password),
+        h: common_vendor.unref(errors).password
+      }, common_vendor.unref(errors).password ? {
+        i: common_vendor.t(common_vendor.unref(errors).password)
+      } : {}, {
+        j: common_vendor.unref(errors).password ? 1 : "",
+        k: common_vendor.o([($event) => common_vendor.isRef(code) ? code.value = $event.detail.value : code = $event.detail.value, ($event) => clearError("code")]),
+        l: common_vendor.unref(code),
+        m: common_vendor.unref(codeImage)
       }, common_vendor.unref(codeImage) ? {
-        i: common_vendor.unref(codeImage),
-        j: common_vendor.o(
+        n: common_vendor.unref(codeImage),
+        o: common_vendor.o(
           //@ts-ignore
           (...args) => common_vendor.unref(api_auth.authCaptcha) && common_vendor.unref(api_auth.authCaptcha)(...args)
         )
       } : {
-        k: common_vendor.t(_ctx.$t("刷新")),
-        l: common_vendor.o(
+        p: common_vendor.o(
           //@ts-ignore
           (...args) => common_vendor.unref(api_auth.authCaptcha) && common_vendor.unref(api_auth.authCaptcha)(...args)
         )
       }, {
-        m: common_vendor.o(handleLogin),
-        n: common_vendor.o(jumpRegister)
+        q: common_vendor.unref(errors).code
+      }, common_vendor.unref(errors).code ? {
+        r: common_vendor.t(common_vendor.unref(errors).code)
+      } : {}, {
+        s: common_vendor.unref(errors).code ? 1 : "",
+        t: common_vendor.o(handleLogin),
+        v: common_vendor.o(jumpRegister)
       });
     };
   }
